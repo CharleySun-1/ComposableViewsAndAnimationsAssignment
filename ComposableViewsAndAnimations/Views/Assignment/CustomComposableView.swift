0d1138925eaaa38890265d5ea94157c3d6ca23b4
CharleySun <Ysun@lcs.on.ca>
2021-03-01 07:31:08 -0500

    Added a scale factor for the image and a when to use animation statement.

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -9,26 +9,34 @@ import SwiftUI
 
 struct CustomComposableView: View {
     
+    // MARK: Stored properties
+    
+    // What to say?
     var message: String
-
+    
+    // Scale factor for the entire image
+    @State private var scaleFactor: CGFloat = 1.0
+    
+    // Whether to apply the animation
+    @State private var useAnimation = false
+    
+   
+    // Initialize a timer that will fire in one second
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         
-     
-        
-            
-            ZStack {
-    
-                
-                
-            }
-            
-            
-        }
-        
-    }
-
-struct CustomComposableView_Previews: PreviewProvider {
-    static var previews: some View {
-        CustomComposableView(message: "whatever")
+        Text("\(message)")
+            .font(Font
+                    .custom("Futura-Bold", size: 26.0, relativeTo: .largeTitle))
+       
     }
 }
+    
+    
+    struct CustomComposableView_Previews: PreviewProvider {
+        static var previews: some View {
+            CustomComposableView(message: "whatever")
+        }
+    }
+
